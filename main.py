096737843914dde510336f743262e6ca9ee6f0f3
Qusai GAZAWY <92310650+cando-jo@users.noreply.github.com>
2024-09-07 13:04:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,25 +8,36 @@ TITLE = "Hayvan Avcısı"
 arkaplan = Actor("arkaplan")
 zurafa = Actor("zürafa", (150, 250))
 puan = 0
-artis_miktari = 100
-
+artis_miktari = 1000
+mod = "menu"
 bonus1 = Actor("bonus", (450, 100))
 bonus2 = Actor("bonus", (450, 200))
+oyna = Actor("oyna", (300, 100))
 
 def draw():
-    arkaplan.draw()
-    zurafa.draw()
-    screen.draw.text(puan, center=(150, 100), color="white", fontsize=96)
-    bonus1.draw()
-    bonus2.draw()
-    screen.draw.text("Her 2 saniye 1$", center=(450, 75), color="black", fontsize=19)
-    screen.draw.text("Ücret: 15 Puan", center=(450, 110), color="black", fontsize=19)
-    screen.draw.text("Her 2 saniye 15$", center=(450, 175), color="black", fontsize=19)
-    screen.draw.text("Ücret: 200 Puan", center=(450, 210), color="black", fontsize=19)
+    if mod == "menu":
+        arkaplan.draw()
+        oyna.draw()
+        screen.draw.text(puan, center=(30, 30), color="white", fontsize=30)
+    if mod == "oyun":
+        arkaplan.draw()
+        zurafa.draw()
+        screen.draw.text(puan, center=(150, 100), color="white", fontsize=96)
+        bonus1.draw()
+        bonus2.draw()
+        screen.draw.text("Her 2 saniye 1$", center=(450, 75), color="black", fontsize=19)
+        screen.draw.text("Ücret: 15 Puan", center=(450, 110), color="black", fontsize=19)
+        screen.draw.text("Her 2 saniye 15$", center=(450, 175), color="black", fontsize=19)
+        screen.draw.text("Ücret: 200 Puan", center=(450, 210), color="black", fontsize=19)
 
 def on_mouse_down(button, pos):
     global puan
+    global mod
     if button == mouse.LEFT:
+        if oyna.collidepoint(pos):
+            mod = "oyun"
+            
+        # eger carpiya tiklarsak modu menu yapcaz
         if zurafa.collidepoint(pos):
             puan += artis_miktari
             zurafa.y = 200
